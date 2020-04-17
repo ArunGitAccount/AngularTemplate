a75658e006305da9098e65706d612eb8c0a9ed4b
Arun Balu <arunece1991@gmail.com>
2020-04-18 02:55:24 +0530

    First Checkin

--- AngularTemplate/ClientApp/src/app/app.component.ts
+++ AngularTemplate/ClientApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
+  //templateUrl: '<app-server></app-server>'
 })
 export class AppComponent {
   title = 'my arun app';
---
+++ AngularTemplate/ClientApp/src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.scss']
+})
+/** server component*/
+export class ServerComponent {
+    /** server ctor */
+    constructor() {
+
+    }
+}
