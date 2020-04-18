a0093f170f082dca90dd3fd5ae8ec7f33bae764e
Arun Balu <arunece1991@gmail.com>
2020-04-19 01:31:39 +0530

    Data Binding

--- AngularTemplate/ClientApp/src/app/home/home.component.ts
+++ AngularTemplate/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls:['./home.component.css'],
+//  styles: [`
+//h3{
+//color: red;
+//}`]
 })
 export class HomeComponent {
   title = 'my arun app';
--- AngularTemplate/ClientApp/src/app/server/server.component.ts
+++ AngularTemplate/ClientApp/src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
@@ -7,8 +7,23 @@
 })
 /** server component*/
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = "Testserver";
     /** server ctor */
-    constructor() {
-
-    }
+  constructor() {
+    setTimeout(() => { this.allowNewServer = true }, 5000);
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!! The name is " + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    //console.log(event); 
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
